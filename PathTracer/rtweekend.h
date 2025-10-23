07861e5503dfcad0afafddffb98151af0af6fadd
Kabir-Hussain68 <158817625+Kabir-Hussain68@users.noreply.github.com>
2025-10-23 13:55:44 +0500

    Add files via upload

---
+++ PathTracer/rtweekend.h
@@ -0,0 +1,45 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <cstdlib>
+
+
+// C++ Std Usings
+
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees)
+{
+    return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    return std::rand() / (RAND_MAX + 1.0f);
+}
+
+inline double random_double(double min, double max)
+{
+    return min + (max - min) * random_double();
+}
+
+// Common Headers
+
+#include "interval.h"
+#include "vec3.h"
+#include "ray.h"
+#include "color.h"
+
+#endif
